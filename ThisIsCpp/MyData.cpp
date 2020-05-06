d5f230861ed98858db938a1678d80b2eb35d4360
Simon-Neo <simonmatthew@naver.com>
2020-05-06 17:40:43 +0900

    Chapter 05 Practice Intro

--- ThisIsCpp/MyData.cpp
+++ ThisIsCpp/MyData.cpp
@@ -1,52 +1,26 @@
 #include "stdafx.h"
 #include "MyData.h"
 
-
-CMyData::CMyData(int iData, const char* pszName)
-	:m_iNum(iData), m_pszName(pszName)
+CMyData::CMyData(int iNum)
+	:m_iNum(iNum)
 {
-	cout << "int iData, const char* pszName - " << m_pszName << endl;
-}
-
-CMyData::CMyData(const CMyData & rhs)
-	: m_iNum(rhs.m_iNum), m_pszName(rhs.m_pszName)
-{
-	cout << "CMyData::CMyData(const CMyData & rhs) -" << m_pszName << endl;
-}
-
-CMyData::CMyData(CMyData && rrhs)
-	:m_iNum(rrhs.m_iNum), m_pszName(rrhs.m_pszName)
-{
-	cout << "CMyData::CMyData(CMyData && rrhs)-" << m_pszName << endl;
+	cout << "CMyData::CMyData(int iNum)" << m_iNum << endl;
 }
 
 CMyData::~CMyData()
 {
-	
-	cout << "CMyData::~CMyData()-" << m_pszName << endl;
-	Release();
+	cout << "CMyData::~CMyData()" << m_iNum <<  endl;
 }
 
-CMyData & CMyData::operator=(const CMyData & rhs)
+CMyData::CMyData(const CMyData & rhs)
+	:m_iNum(rhs.m_iNum)
 {
-	m_iNum = rhs.m_iNum;
-	//m_pszName = rhs.m_pszName;
-
-	cout << "CMyData & CMyData::operator=(const CMyData & rhs)" << m_pszName << endl;
-	return *this;
+	cout << "CMyData::CMyData(const CMyData & rhs)" << endl;
 }
 
-void CMyData::Release()
+CMyData::CMyData(CMyData && rrhs)
+	:m_iNum(rrhs.m_iNum)
 {
-	m_pszName = nullptr;
+	cout << "CMyData::CMyData(CMyData && rrhs)" << m_iNum << endl;
 }
 
-void CMyData::Render()
-{
-	cout << "Num = " << m_iNum << "Name = " << m_pszName << endl;
-}
-
-//CMyData::CMyData(const CMyData & rhs)
-//{
-//	cout << "CMydata(const CMydata& rhs)" << endl;
-//}
--- ThisIsCpp/MyData.h
+++ ThisIsCpp/MyData.h
@@ -3,24 +3,24 @@
 class CMyData
 {
 public:
-	CMyData(int iData, const char* pszName);
+	CMyData(int iNum);
 	~CMyData();
 	
 	CMyData(const CMyData& rhs);
 
 	CMyData(CMyData&& rrhs);
 
-	CMyData& operator= (const CMyData& rhs);
-	
+	operator int() { return m_iNum; }
+
+
+public:
 	const int GetData() const { return m_iNum; }
 public:
 	//void SetData(const int iData) { m_iData = iData; }
 	//int GetData() const { return m_iData; }
 
-	void Release();
-	void Render();
+	void RenderData() { cout << m_iNum << endl; }
 private:
 	int m_iNum = 0;
-	const char* m_pszName = nullptr;
 };
 
